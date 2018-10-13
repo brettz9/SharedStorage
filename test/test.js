bfc1e5a9be7115f90d1a911cb04b48563e1fb6ea
Brett Zamir <brettz9@yahoo.com>
2018-10-13 11:36:40 +0800

    - Begin test set-up

---
+++ test/test.js
@@ -0,0 +1,12 @@
+
+const ifr = document.createElement('iframe');
+ifr.style.display = 'none';
+ifr.addEventListener('load', () => {
+  console.log('loaded');
+  ifr.contentWindow.postMessage({
+    isSharedStorage: true,
+    getMaxRemaining: true
+  }, '*');
+});
+ifr.src = 'http://localhost:8050';
+document.body.append(ifr);
