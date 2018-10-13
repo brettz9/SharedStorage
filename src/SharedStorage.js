07a0c812daf49d3a9783d9d45c44a46bec04a2fd
Brett Zamir <brettz9@yahoo.com>
2018-10-13 11:08:03 +0800

    - Todos/Code org. - i18n - Public API work - Require `isSharedStorage`

--- src/SharedStorage.js
+++ src/SharedStorage.js
@@ -1,5 +1,42 @@
 // This is for user API (to be built-in to the browser but as module for now)
+const iframeSource = 'http://localhost:8051'; // 'https://shared-storage.org';
+const promises = {};
+let i = 0;
+
+const iframePost = (msgObj) => {
+  const iframe = document.createElement('iframe');
+  iframe.style.display = 'none';
+  iframe.addEventListener('load', () => {
+    iframe.postMessage(Object.assign(msgObj, {i, isSharedStorage: true}), new URL(iframeSource).origin);
+    iframe.remove();
+  });
+  iframe.src = iframeSource;
+  document.body.append(iframe);
+};
+const iframePostPromise = (msgObj) => {
+  return new Promise((resolve, reject) => {
+    iframePost(msgObj);
+    promises[i++] = {resolve, reject};
+  });
+};
+
 export default { // `SharedStorage`
-  async get () {},
-  async save () {}
+  async get ({namespace}) {
+    return iframePostPromise({
+      namespacing: 'origin-top', // or 'origin-children' or not present
+      namespace
+    });
+  },
+  async getMaxRemaining () {
+    return iframePostPromise({
+      getMaxRemaining: true
+    });
+  },
+  async save ({data, namespace}) {
+    return iframePostPromise({
+      namespacing: 'origin-top', // or 'origin-children' or not present
+      namespace,
+      data
+    });
+  }
 };
